809cce7a9faf92a33813874d711fa13991d5abf9
Aal-aa <aalaa.waleed.a@gmail.com>
2024-12-15 17:29:19 +0200

    testttt

---
+++ main.js
@@ -0,0 +1,10 @@
+function popup2() {
+    window.alert("Our predatory animals feature unique intelligence, which enhances the strength of training.🦁")
+}
+function popup1() {
+    window.alert("We train predators professionally and safely 🥺")
+}
+function popup() {
+    window.alert("Elhelw is my famliy ❤️")
+
+}
